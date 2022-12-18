a7e25e954faf4553f381e4c17a891cd376fde52d
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-12-18 17:37:09 +0800

    Fixed bug on author/book views by rewriting database

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,7 +3,7 @@ const Author = require("../models/author");
 // Display list of all Authors.
 exports.author_list = (req, res,next) => {
   Author.find()
-    .sort(["family_name","ascending"])
+    .sort({"family_name":1})
     .exec(function(err, list_authors){
       if (err) {
         return next(err);
