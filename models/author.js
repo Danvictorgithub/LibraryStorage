b305780eb8fa548a7e68e154fde877db0ff7ce00
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-12-18 18:28:22 +0800

    Added Genre Detail View

--- models/author.js
+++ models/author.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-
+const {DateTime} = require("luxon");
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   family_name: { type: String, required: true, maxLength: 100 },
@@ -28,6 +28,11 @@ AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
-
+AuthorSchema.virtual("date_of_birth_formatted").get(function() {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+AuthorSchema.virtual("date_of_death_formatted").get(function() {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
