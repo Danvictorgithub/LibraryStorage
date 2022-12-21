7fd73d52168d76bf4707569fc6bd45e3600ee99d
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-12-21 18:19:55 +0800

    Added Author Delete View

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -107,13 +107,61 @@ exports.author_create_post = [
   ];
 
 // Display Author delete form on GET.
-exports.author_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete GET");
+exports.author_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books(callback) {
+        Book.find({author:req.params.id}).exec(callback);
+      }
+    },(err,results)=>{
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        res.redirect("/catalog/authors");
+      }
+      res.render("author_delete", {
+        title:"Delete Author",
+        author: results.author,
+        author_books:results.authors_books,
+      });
+    });
 };
 
 // Handle Author delete on POST.
-exports.author_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+exports.author_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.body.authorid).exec(callback);
+      },
+      authors_books(callback) { 
+        Book.find({author:req.body.authorid}).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.authors_books.length > 0) {
+        res.render("author_delete", {
+          title: "Delte Author",
+          author: results.author,
+          author_books:results.authors_books,
+        });
+        return;
+      }
+      Author.findByIdAndRemove(req.body.authorid,(err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/catalog/authors");
+      });
+    }
+  );
 };
 
 // Display Author update form on GET.
