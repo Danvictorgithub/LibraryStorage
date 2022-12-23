2ee61757dcd834822605726a1b7b243dbcf579e6
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-12-23 13:39:08 +0800

    Added Author and BookInstance Update View

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -165,11 +165,67 @@ exports.author_delete_post = (req, res, next) => {
 };
 
 // Display Author update form on GET.
-exports.author_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
+exports.author_update_get = (req, res, next) => {
+  Author.findById(req.params.id).exec((err,theauthor)=> {
+    if (err) {
+      return next(err);
+    }
+    if (theauthor == null) {
+      const err = new Error("Author not Found");
+      err.status = 404;
+      return next(err);
+    }
+    res.render("author_form", {title: "Update Author",author:theauthor});
+  });
+  
 };
 
 // Handle Author update on POST.
-exports.author_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
-};
+exports.author_update_post = [
+  body("family_name")
+    .trim()
+    .isLength({min:1})
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({min: 1})
+    .escape()
+    .withMessage("Family name must be specified")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters"),
+  body("date_of_birth","Invalid date of birth")
+    .optional({checkFalsy: true})
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  (req,res,next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Update Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    }
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id,
+    });
+    Author.findByIdAndUpdate(req.params.id,author,{},(err,theauthor)=>{
+      if (err) {
+        return next(err);
+      }
+      res.redirect(theauthor.url);
+    });
+  }
+];
--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,6 +1,7 @@
 const BookInstance = require("../models/bookinstance");
 const {body, validationResult} = require("express-validator");
 const Book = require("../models/book");
+const async = require("async");
 // Display list of all BookInstances.
 exports.bookinstance_list = (req, res) => {
   BookInstance.find()
@@ -125,11 +126,79 @@ exports.bookinstance_delete_post = (req, res, next) => {
 };
 
 // Display BookInstance update form on GET.
-exports.bookinstance_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance update GET");
+exports.bookinstance_update_get = (req, res, next) => {
+  async.parallel(
+    {
+      bookinstance(callback) {
+        BookInstance.findById(req.params.id).exec(callback);
+      },
+      books(callback) {
+        Book.find({}, "title").exec(callback);
+      }
+    },
+    (err,results) =>{
+      if (err) {
+        return next(err);
+      }
+      if (results.bookinstance == null) {
+        const err = new Error("BookInstance Not Found");
+        err.status = 404;
+        return next(err);
+      }
+      res.render("bookinstance_form", {
+        title:"Update BookInstance",
+        book_list:results.books,
+        bookinstance:results.bookinstance
+      });
+    });
 };
 
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance update POST");
-};
+exports.bookinstance_update_post = [
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),  (req,res,next) => {
+    const errors = validationResult(req);
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id:req.params.id,
+    });
+    if (!errors.isEmpty()) {
+      async.parallel(
+      {
+      bookinstance(callback) {
+        BookInstance.findById(req.params.id).exec(callback);
+      },
+      books(callback) {
+        Book.find({}, "title").exec(callback);
+      }
+      },
+      (err,results) =>{
+        if (err) {
+          return next(err);
+        }
+        res.render("bookinstance_form", {
+          title:"Update BookInstance",
+          book_list:results.books,
+          bookinstance:results.bookinstance
+        });
+      });
+    }
+    BookInstance.findByIdAndUpdate(req.params.id,bookinstance,{},(err,thebookinstnace)=> {
+      if (err) {
+        return next(err);
+      }
+      res.redirect(thebookinstnace.url);
+    });
+  }
+];
