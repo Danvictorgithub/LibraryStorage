102511e7b3adf14b8a13171ca329c4c97519ef37
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-12-23 15:42:43 +0800

    Added Helmet and Compression for Security and Performance

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 let catalogRouter = require('./routes/catalog');
+const compression = require("compression");
+const helmet = require("helmet");
 var app = express();
 // Set up mongoose
 const mongoose = require("mongoose");
@@ -19,10 +21,13 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/catalog',catalogRouter);
